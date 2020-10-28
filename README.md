3865fdb8c1a92de66c4a1f16760a8178f43a3412
Valentine Erokhin <saviorand@gmail.com>
2020-10-28 11:30:35 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Notion Layout - Chrome Extension
+## What is it?
+Chrome Extension that makes viewing Notion database pages better!
+
+It auto-hides page properties list that often pushes down the page content.
+
+## How it works?
+By default list of properties is collapsed:             |  Clicking on the bar opens/hides the list of properties:
+:-------------------------:|:-------------------------:
+![collapsed](https://github.com/alexander-kazakov/notion-layout-extension/raw/master/img/collapsed.png)  |  ![uncollapsed](https://github.com/alexander-kazakov/notion-layout-extension/raw/master/img/uncollapsed.png)
+
+## How to install?
+1. Download the whole repo as Zip file (see green button "Code" in top right of this page)
+2. Open Chrome Extension chrome://extensions
+3. Enable "Developer Mode" (top right)
+4. Unzip the downloaded folder
+5. Drag & drop the unzipped folder into Chrome extensions window
+6. Enjoy
+
+YouTube video which shows steps 2-5: https://www.youtube.com/watch?v=5W9ZPEaVMWE
+
+## Safety
+This is a simple extension with <2 pages of code. You can read it entirely to make sure it is safe. 
+PS. I include official copy of jQuery library as I didn't figure out how to reference it via URL in the manifest file.
+
+## Notice
+I wrote it in an afternoon for myself and sharing with the community. It is not affiliated with Notion the company.
+
+If you find a bug, you can log it here: https://github.com/alexander-kazakov/notion-layout-extension/issues
---
+++ content.js
@@ -0,0 +1,64 @@
+var isVisible=false
+
+function setPropertiesListVisibility(propertiesList, visible){
+    if (visible){
+        // Show properties list section
+        propertiesList.style.height = null;
+        propertiesList.style.display = null;
+        isVisible = true
+    }
+    else {
+        // Hide properties list section
+        propertiesList.style.height = 0;
+        propertiesList.style.display = "none";
+        isVisible = false
+    }
+}
+
+// Called every time something new loads inside Notion window
+// e.g. you navigate to a different Notion page
+var onPageChange = function(){
+    //console.log("Notion Layout Improver: Something changed");
+
+    // Find the block with properties list
+    var propertiesLists = document.querySelectorAll(".notion-scroller.vertical > div:nth-child(2) > div > div:nth-child(1)[style='width: 100%; font-size: 14px;']");
+
+    // For each found properties list
+    propertiesLists.forEach(function(propertiesList){
+        //console.log("Found properties list");
+
+        // Set up the toggle button
+        let toggleButton = document.createElement("button");
+        //let toggleIcon = document.createElement("i");
+        //toggleIcon.setAttribute('class', 'far fa-eye');
+        //toggleButton.appendChild(toggleIcon);
+        let buttonText = "👁 Show properties";
+        toggleButton.setAttribute('class', 'propertiesToggleBar');
+        toggleButton.innerHTML = buttonText;
+        toggleButton.onclick = function(){
+
+            setPropertiesListVisibility(propertiesList, !isVisible);
+            (buttonText === "👁 Show properties") ? (buttonText = "Hide properties") : buttonText = "👁 Show properties";
+            toggleButton.innerHTML = buttonText;
+
+            }
+
+        // If not already processed this properties list,
+        // add the toggle button and hide the list
+        if (!(propertiesList.id === "already_processed")){
+            //console.log("Notion Layout Improver: Processing new properties list");
+            var parentBlockForButton = propertiesList.parentElement
+            parentBlockForButton.prepend(toggleButton)
+            setPropertiesListVisibility(propertiesList, false)
+            propertiesList.id = "already_processed";
+
+        }
+    })
+}
+
+// This calls onPageChange function each time Notion window changes
+// e.g. you navigate to a new Notion page
+const targetNode = document.body;
+const config = {attributes: false, childList: true, subtree: true };
+const observer = new MutationObserver(onPageChange);
+observer.observe(targetNode, config);
